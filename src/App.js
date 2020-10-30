1ffeecd9dc5130d62fed5a0ab4de90dc9e2e4152
Jacob Ruiz <jacobruizdesign@gmail.com>
2020-10-30 11:08:55 +1000

    Make copy change

--- src/App.js
+++ src/App.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './styles.css';
 
 export default function App() {
-  return <div className="App">Testing github.</div>;
+  return <div className="App">Added gitignore.</div>;
 }
