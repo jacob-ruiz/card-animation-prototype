d1758ead5d1afdca0f54641100ef360b65ab40e6
Jacob Ruiz <jacobruizdesign@gmail.com>
2020-10-31 20:49:45 +1000

    clean up logic

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -3,27 +3,46 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Card from '../Card/Card';
 import './styles.css';
 
+const initialList = [
+  { id: 1, text: 'Suggestion 1' },
+  { id: 2, text: 'Suggestion 2' },
+  { id: 3, text: 'Suggestion 3' },
+  { id: 4, text: 'Suggestion 4' },
+  { id: 5, text: 'Suggestion 5' },
+  { id: 6, text: 'Suggestion 6' },
+  { id: 7, text: 'Suggestion 7' },
+  { id: 8, text: 'Suggestion 8' },
+];
+
 function CardList() {
-  const [items, setItems] = useState([
-    { id: 1, text: 'Suggestion 1' },
-    { id: 2, text: 'Suggestion 2' },
-    { id: 3, text: 'Suggestion 3' },
-    { id: 4, text: 'Suggestion 4' },
-    { id: 5, text: 'Suggestion 5' },
-    { id: 6, text: 'Suggestion 6' },
-    { id: 7, text: 'Suggestion 7' },
-    { id: 8, text: 'Suggestion 8' },
-  ]);
-  const [activeItem, setActiveItem] = useState(1);
+  const [items, setItems] = useState(initialList);
+  const [activeItem, setActiveItem] = useState(items[0].id);
 
-  function removeItem(id) {
-    const newList = items.filter((item) => item.id !== id);
+  function handleRemove(id) {
+    let i;
+    const newList = items.filter((item, index) => {
+      if (item.id === id) {
+        i = index;
+      }
+      return item.id !== id;
+    });
 
-    setItems(newList);
-
-    if (items.length) {
-      setActiveItem(items[0].id);
+    // Here, we figure out which card to open next
+    // We need to find the id of the card after the current card
+    // If there is no "next card", we check if there is a card before
+    // If there is neither, then we're dismissing the next card
+    let newItemID;
+    const itemAfter = items[i + 1];
+    const itemBefore = items[i - 1];
+    if (itemAfter) {
+      newItemID = itemAfter.id;
+    } else if (itemBefore) {
+      newItemID = itemBefore.id;
+    } else {
+      setActiveItem(null);
     }
+    setActiveItem(newItemID);
+    setItems(newList);
   }
 
   return (
@@ -31,12 +50,10 @@ function CardList() {
       {items.map(({ id, text }) => (
         <Card
           key={id}
-          onExited={() => removeItem(id)}
+          id={id}
           isActive={activeItem === id}
-          onClick={() => {
-            if (activeItem === id) return;
-            setActiveItem(id);
-          }}
+          setActiveItem={setActiveItem}
+          onRemove={handleRemove}
         >
           {text}
         </Card>
